3d6be16a297ec7fc998e9d4fba958d991e33026a
Mohith Kumar <155897668+b0tProgrammer@users.noreply.github.com>
2024-12-03 23:39:38 +0530

    Add files via upload

---
+++ Balanced Tunnel.java
@@ -0,0 +1,258 @@
+
+import javax.print.attribute.standard.ColorSupported;
+import java.util.*;
+import java.io.*;
+
+import static java.lang.Math.*;
+
+public class Main {
+
+    private static final long lif = (long) 1e9 + 7; //long infinity
+    private static final int iif = 998244353; // int infinity!
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+
+//            Comparator<Pair> com = Comparator.comparingInt(i -> i.v1);
+
+            int testcases = 1;
+
+//            testcases = in.nextInt();
+
+            while (testcases-- > 0) {
+
+                /*
+                    how will you find that the vehicle has overTaken another vehicle?
+
+
+                    We use an arrray to store the indices where the entry elements are ocuured! And we will check for every vehicle outside the tunnel.
+                    that the element is overTaken or not! This can be done by checking the current vehicles position with the vehicle's position in the entry array.
+                    And if the current vehicles position is greater than the entry vehicles position it states that the current vehicle overtook the entry array element.
+                    Else the next element should be in the order of the entry element
+                 */
+
+                int siz = in.nextInt();
+                int[] entry = new int[siz+1];
+                int[] entryPos = new int[siz+1];
+                for(int i = 1; i <= siz; i++) {
+                    entry[i] = in.nextInt();
+                    entryPos[entry[i]] = i;
+                }
+                int overTaken = 0;
+                int toCome = 1;
+                Set<Integer> set = new HashSet<>();
+                for(int i = 1; i <= siz; i++) {
+                    int currVal = in.nextInt();
+                    set.add(entryPos[currVal]);
+                    if(entryPos[currVal] > toCome) {
+                        overTaken++;
+                    }
+                    else if(entryPos[currVal] == toCome) {
+                        while(!set.isEmpty() && set.contains(toCome)) {
+                            set.remove(toCome);
+                            toCome++;
+                        }
+                    }
+                }
+                out.println(overTaken);
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    //<--------------------------------------Fast reader----------------------------------------->
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+//<---------------------------------------Fast writer--------------------------------------------->
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println() throws IOException {
+            bw.append("\n");
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+// <-------------------------------------------------Some helper methods---------------------------------------->
+
+    private static long kadane(int l, int r, int[] nums) {
+        long sum = 0;
+        long mSum = 0;
+        for(int i = l; i < r; i++) {
+            if(sum < 0) sum = nums[i];
+            else sum += nums[i];
+            mSum = max(sum,mSum);
+        }
+        return mSum;
+    }
+
+    public static List<Integer> reversed(List<Integer> lis) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = lis.size()-1; i >= 0; i--) {
+            ans.add(lis.get(i));
+        }
+        return ans;
+    }
+
+    private static int update(int ans, int i) {
+        return (int)((ans*1L*i)%iif);
+    }
+
+    private static void getFactors(int num, Map<Integer, Integer> map) {
+
+        for(int i = 2; i <= (int) Math.sqrt(num); i++) {
+            while(num%i == 0) {
+                map.put(i,map.getOrDefault(i,0)+1);
+                num /= i;
+            }
+        }
+        if(num > 1) map.put(num,map.getOrDefault(num,0)+1);
+    }
+
+    public static void print(boolean cond) {
+        System.out.println(cond ? "Yes" : "No");
+    }
+
+    public static long fastPow(long b, long e) {
+        long res = 1;
+        while (e > 0) {
+            if (odd(e)) res = (res * b) % lif;
+            b = (b * b) % lif;
+            e = e >> 1;
+        }
+        return res;
+    }
+
+    public static int charToInt(char c) {
+        return (c - '0');
+    }
+
+    public static char intToChar(int n) {
+        return (char) (n + 48);
+    }
+
+    public static int[] sieve(int upto) {
+        int[] primes = new int[upto + 1];
+        Arrays.fill(primes, 1);
+        for (int i = 2; i * i <= upto; i++) {
+            if (primes[i] == 1)
+                for (int j = i * i; j <= upto; j += i) {
+                    primes[j] = 0;
+                }
+        }
+        return primes;
+    }
+
+    private static long gcd(long a, long b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b != 0) return gcd(b, (a % b));
+        return a;
+    }
+
+    private static long getSum(int startNum, int commonDiff, long range) {
+        long lastNum = startNum + (range - 1) * commonDiff;
+        return range * (startNum + lastNum) / 2;
+    }
+
+    private static boolean odd(long siz) {
+        return (siz & 1) == 1;
+    }
+
+    private static boolean powOf2(long siz) {
+        return (siz & (siz - 1)) == 0;
+    }
+
+    private static List<Integer> primeFactors(int siz) {
+
+        List<Integer> sizs = new ArrayList<>();
+        int[] p;
+        p = sieve(siz);
+        for (int i = 2; i <= siz; i++) {
+            while (siz % i == 0 && p[i] == 1) {
+                sizs.add(i);
+                siz /= i;
+            }
+        }
+        return sizs;
+    }
+}
+
+class Pair2 {
+    long v1;
+    long v2;
+
+    public Pair2(long v1, long v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+}
+class Pair {
+    int v1,v2;
+    public Pair(int v,int w) {v1 = v; v2 = w;}
+}
